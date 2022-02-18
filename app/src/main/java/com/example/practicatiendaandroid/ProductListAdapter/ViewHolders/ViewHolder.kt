cd07240d1186f50cb0dcec64ee5dc1867a7c764f
Álvaro Fernández <avunotfound@gmail.com>
2022-02-18 14:28:42 +0100

    contentdialog

--- app/src/main/java/com/example/practicatiendaandroid/ProductListAdapter/ViewHolders/ViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.example.practicatiendaandroid.ProductListAdapter.ViewHolders
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-import com.example.practicatiendaandroid.databinding.ListElementLayoutBinding
-
-class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
-        val binding = ListElementLayoutBinding.bind(view)
-}
--- app/src/main/java/com/example/practicatiendaandroid/ui/Fragments/ProductList.kt
+++ app/src/main/java/com/example/practicatiendaandroid/ui/Fragments/ProductList.kt
@@ -1,38 +1,92 @@
 package com.example.practicatiendaandroid.ui.Fragments
 
 import android.os.Bundle
-import android.os.Debug
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
-import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.practicatiendaandroid.Clases.Product
 import com.example.practicatiendaandroid.ProductListAdapter.ProductAdapter
 import com.example.practicatiendaandroid.R
 import com.example.practicatiendaandroid.databinding.FragmentProductListBinding
 import com.example.practicatiendaandroid.ui.ViewModels.ProductListVM
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.transition.MaterialContainerTransform
-import com.google.android.material.transition.platform.Hold
 
-private fun iniList(): ArrayList<Product>
-{
-    val tempList:ArrayList<Product> = ArrayList()
-    tempList.add(0, Product(1,"Fantastic Granite Bench",23F, 23F,"Outdoors, Tools & Toys","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPcis2nSFZAO2nG4enJj0xxHBgVkxTuiXukg&usqp=CAU"))
-    tempList.add(1, Product(2,"Totoro uwu",12F, 0.56F,"Clothing & Games","https://cdn.shopify.com/s/files/1/0424/3544/4900/products/product-image-1585079422.jpg?v=1623132447"))
-    tempList.add(2, Product(3,"Silla gamer",223F, 223F,"Sports","https://pbs.twimg.com/media/FLVCGcuXoAARVgi?format=jpg&name=large"))
-    tempList.add(3, Product(4,"Silksong't",42.5F, 23F,"Sports","https://pbs.twimg.com/media/FGN-4ouXwAA5ePY?format=jpg&name=small"))
-    tempList.add(4, Product(3,"Silla gamer",223F, 223F,"Sports","https://pbs.twimg.com/media/FLVCGcuXoAARVgi?format=jpg&name=large"))
-    tempList.add(5, Product(4,"Silksong't",42.5F, 23F,"Sports","https://pbs.twimg.com/media/FGN-4ouXwAA5ePY?format=jpg&name=small"))
+private fun iniList(): ArrayList<Product> {
+    val tempList: ArrayList<Product> = ArrayList()
+    tempList.add(
+        0,
+        Product(
+            1,
+            "Fantastic Granite Bench",
+            23F,
+            23F,
+            "Outdoors, Tools & Toys",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPcis2nSFZAO2nG4enJj0xxHBgVkxTuiXukg&usqp=CAU"
+        )
+    )
+    tempList.add(
+        1,
+        Product(
+            2,
+            "Totoro uwu",
+            12F,
+            0.56F,
+            "Clothing & Games",
+            "https://cdn.shopify.com/s/files/1/0424/3544/4900/products/product-image-1585079422.jpg?v=1623132447"
+        )
+    )
+    tempList.add(
+        2,
+        Product(
+            3,
+            "Silla gamer",
+            223F,
+            223F,
+            "Sports",
+            "https://pbs.twimg.com/media/FLVCGcuXoAARVgi?format=jpg&name=large"
+        )
+    )
+    tempList.add(
+        3,
+        Product(
+            4,
+            "Silksong't",
+            42.5F,
+            23F,
+            "Sports",
+            "https://pbs.twimg.com/media/FGN-4ouXwAA5ePY?format=jpg&name=small"
+        )
+    )
+    tempList.add(
+        4,
+        Product(
+            3,
+            "Silla gamer",
+            223F,
+            223F,
+            "Sports",
+            "https://pbs.twimg.com/media/FLVCGcuXoAARVgi?format=jpg&name=large"
+        )
+    )
+    tempList.add(
+        5,
+        Product(
+            4,
+            "Silksong't",
+            42.5F,
+            23F,
+            "Sports",
+            "https://pbs.twimg.com/media/FGN-4ouXwAA5ePY?format=jpg&name=small"
+        )
+    )
 
     return tempList
 }
@@ -40,10 +94,11 @@ private fun iniList(): ArrayList<Product>
 class ProductList : Fragment() {
     private lateinit var productsList: ArrayList<Product>
     private lateinit var navController: NavController
-    private val viewModel:ProductListVM by activityViewModels()
-    private var auxBinding:FragmentProductListBinding?=null
-    private val valBind get()=auxBinding!!
-    private val detailsFragment=DetailsFragment()
+    private val viewModel: ProductListVM by activityViewModels()
+    private var auxBinding: FragmentProductListBinding? = null
+    private val valBind get() = auxBinding!!
+    private val detailsFragment = DetailsFragment()
+    private lateinit var categoriesList : ArrayList<String>
 //    private lateinit var filterButton:FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,13 +108,21 @@ class ProductList : Fragment() {
 //        exitTransition = Hold()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         auxBinding = FragmentProductListBinding.inflate(inflater, container, false)
-            productsList= iniList()
+        productsList = iniList()
+        productsList.forEach {
+            val category=it.category
+            if(categoriesList.contains(category))
+                categoriesList.add(category)
+        }
 //        filterButton=valBind.fragmentProductListFab
 //        filterButton.setOnClickListener {
-//            val filterDialog=layoutInflater.inflate(R.layout.filter_dialog_layout,container, false)
-//
+
 ////            TODO("CREATE AND LAUNCH CONTENTDIALOG")
 //        }
         return valBind.root
@@ -67,13 +130,42 @@ class ProductList : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        viewModel.productSelected.observe(viewLifecycleOwner, this::onProductoSelected)
+        showFilterDialog()
 
-        navController=findNavController()
+        navController = findNavController()
         valBind.fragmentProductListRecyclerview.apply {
             layoutManager = GridLayoutManager(view.context, 2)
-            adapter = ProductAdapter(productsList){onProductoSelected(it)}
+            adapter = ProductAdapter(productsList) { onProductoSelected(it) }
         }
+    }
+
+    private fun showFilterDialog() {
+        val filterDialog = layoutInflater.inflate(R.layout.filter_dialog_layout, null)
+        //        viewModel.productSelected.observe(viewLifecycleOwner, this::onProductoSelected)
+        val categoriesSpinner =
+            filterDialog.findViewById<Spinner>(R.id.filter_dialog_layout__categories_spinner)
+        val orderBySpinner =
+            filterDialog.findViewById<Spinner>(R.id.filter_dialog_layout__order_criteria_spinner)
+        val orderByAdapter: ArrayAdapter<String> = ArrayAdapter(
+            context!!,
+            android.R.layout.simple_spinner_dropdown_item,
+            mutableListOf("Precio", "Nombre")
+        )
+        val filterAdapter: ArrayAdapter<String> = ArrayAdapter(
+            context!!,
+            android.R.layout.simple_spinner_dropdown_item,
+            categoriesList
+        )
+        orderBySpinner.adapter = orderByAdapter
+        MaterialAlertDialogBuilder(context!!)
+            .setTitle("Filtrar")
+            .setView(filterDialog)
+            .setCancelable(true)
+            .setPositiveButton("Filtrar"){dialogInterface, which->
+                val category:String=categoriesSpinner.selectedItem.toString()
+                val criteria:String=orderBySpinner.selectedItem.toString()
+            }
+            .show()
     }
 
     private fun onProductoSelected(productClicked: Product) {
@@ -85,26 +177,6 @@ class ProductList : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        auxBinding=null
+        auxBinding = null
     }
-
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment ProductList.
-//         */
-//        //Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            ProductList().apply {
-//                arguments = Bundle().apply {
-////                    putString(ARG_PARAM1, param1)
-////                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
 }
