82f28be32f090af6a7481e2c6d648758d8ef36d1
Álvaro Fernández <avunotfound@gmail.com>
2022-02-22 14:39:48 +0100

    amono

--- app/src/main/java/com/example/practicatiendaandroid/Data/DAO/CartDao.kt
+++ app/src/main/java/com/example/practicatiendaandroid/Data/DAO/CartDao.kt
@@ -1,12 +1,17 @@
 package com.example.practicatiendaandroid.Data.DAO
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.practicatiendaandroid.Data.Entities.CartEntity
+import com.example.practicatiendaandroid.Data.Entities.ProductEntity
 
 @Dao
 interface CartDao {
     @Query("SELECT * FROM cart_table")
     suspend fun getCartsList():List<CartEntity>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(product:ProductEntity)
 
 }
