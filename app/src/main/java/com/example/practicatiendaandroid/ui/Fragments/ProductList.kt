efc7c231eb8ed7f8df9f0dd0bc0325fd1fd06277
Álvaro Fernández <avunotfound@gmail.com>
2022-02-24 10:55:20 +0100

    Agradecido con mi profe German

--- app/src/main/java/com/example/practicatiendaandroid/ui/Fragments/ProductList.kt
+++ app/src/main/java/com/example/practicatiendaandroid/ui/Fragments/ProductList.kt
@@ -1,9 +1,7 @@
 package com.example.practicatiendaandroid.ui.Fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -27,33 +25,45 @@ class ProductList : Fragment() {
     private val valBind get() = auxBinding!!
     private val detailsFragment = DetailsFragment()
     private var categoriesList = ArrayList<String>()
+    lateinit var adapter: ProductAdapter
     private lateinit var filterButton: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        arguments?.let {
 //        }
+//        setHasOptionsMenu(true)
     }
 
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        inflater.inflate(R.menu.app_bar_menu, menu)
+//        super.onCreateOptionsMenu(menu, inflater)
+//    }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return when (item.itemId) {
+//            R.id.app_bar_menu__display_options -> {
+//                showFilterDialog()
+//                true
+//            }
+//            else -> true
+//        }
+//    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         auxBinding = FragmentProductListBinding.inflate(inflater, container, false)
-//        filterButton = valBind.fragmentProductListFab
-//        filterButton.setOnClickListener {
-//            showFilterDialog()
-//        }
+        filterButton = valBind.fragmentProductListFab
+        filterButton.setOnClickListener {
+            showFilterDialog()
+        }
         return valBind.root
     }
 
     private fun OnProdListLoaded(productList:List<Product>) {
-        valBind.fragmentProductListRecyclerview.adapter = ProductAdapter(
-            R.layout.material_card_product,
-            productList
-        ) { onProductoSelected(it) }
-        viewModel.vmProdList.value?.forEach {
+        updateListData(productList)
+        productList.forEach {
             val category = it.category
             if (!categoriesList.contains(category))
                 categoriesList.add(category)
@@ -72,7 +82,6 @@ class ProductList : Fragment() {
 
     private fun showFilterDialog() {
         val filterDialog = layoutInflater.inflate(R.layout.filter_dialog_layout, null)
-        //        viewModel.productSelected.observe(viewLifecycleOwner, this::onProductoSelected)
         val orderCriteriaSpinner =
             filterDialog.findViewById<Spinner>(R.id.filter_dialog_layout__order_criteria_spinner)
         val categoriesSpinner =
@@ -89,16 +98,31 @@ class ProductList : Fragment() {
         )
         categoriesSpinner.adapter = filterAdapter
         orderCriteriaSpinner.adapter = orderByAdapter
-        MaterialAlertDialogBuilder(requireContext())
+        MaterialAlertDialogBuilder(context!!)
             .setTitle("Filtrar")
             .setView(filterDialog)
             .setCancelable(true)
-            .setPositiveButton("Filtrar") { dialogInterface, which ->
-                val category: String = orderCriteriaSpinner.selectedItem.toString()
-                val criteria: String = categoriesSpinner.selectedItem.toString()
+            .setPositiveButton("Filtrar") { _, _ ->
+                val criteria: String = orderCriteriaSpinner.selectedItem.toString()
+                val category: String = categoriesSpinner.selectedItem.toString()
+                filterList(criteria, category)
             }
             .show()
     }
+
+    private fun filterList(criteria: String, selectedCategory: String) {
+        val filteredProductlist: List<Product>
+        if(criteria == "Precio") {
+            filteredProductlist= viewModel.vmProdList.value!!.filter { product -> product.category==selectedCategory
+            }.sortedBy { it.price }
+        }
+        else {
+            filteredProductlist= viewModel.vmProdList.value!!.filter { product -> product.category==selectedCategory
+            }.sortedBy { it.productName }
+        }
+        updateListData(filteredProductlist)
+    }
+
     private fun showDetailsDialog(productClicked: Product){
         val detailsDialog = layoutInflater.inflate(R.layout.material_details_card, null)
         val prodImage:ImageView=detailsDialog.findViewById(R.id.material_details_card__product_image)
@@ -127,6 +151,10 @@ class ProductList : Fragment() {
 //        navController.navigate(R.id.action_productList_to_detailsFragment)
     }
 
+    private fun updateListData(prodList:List<Product>){
+        adapter= ProductAdapter(R.layout.material_card_product,prodList){ onProductoSelected(it)}
+        valBind.fragmentProductListRecyclerview.adapter=adapter
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
